9cd77e348e783cc992c33da3e96d644d36fde492
rittidate <rittidate.k@trueclarityintl.com>
2014-01-15 01:26:06 +0700

    login

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,54 +2,65 @@
 
 class Login extends CI_Controller
 {
-	public $user = null;
+	public $user_facebook = null;
 
     public function __construct()
     {
         parent::__construct();
-		//parse_str($_SESSION['QUERY_STRING'], $_REQUEST);
-
         $this->load->library('fb');
-        
-		$this->user = $this->fb->sdk->getUser();
-		//$user = $this->fb->getUser();
-		//var_dump($this->fb->sdk->getUser());
+         $this->user_facebook = $this->fb->sdk->getUser();
+		//parse_str($_SESSION['QUERY_STRING'], $_REQUEST);
     }
 
    public function index()
    {
-   		if($this->user){
-   			  try { 
-				   $user_profile = $this->fb->sdk->api('/me'); // เป็นการเรียก Method /me ซึ่งเป็นข้อมูลเกี่ยวกับผู้ใช้ท่านนั้นๆ ที่ได้ทำการ Login
-				   echo "<br/>";
-				   print_r($user_profile);
-				   //echo $user_profile['email'];
-				   /*
-				   $_SESSION['LOGIN_FB_ID'] = $FB_ME_INFO["id"];
-				   $_SESSION['LOGIN_FB_FULLNAME'] = $FB_ME_INFO["name"];
-				   header("Location:./index.php"); 
-				   */
-			  } catch(FacebookApiException $e) {
-			  	 
-				   echo $e;  // print Error
-				   $this->user = null;
-				   //header("Location:./index.php?Login=fail"); 
-			  }
-   		}
+
 		
-		if($this->user){
-				$logout = $this->fb->sdk->getLogoutUrl(array("next"=>base_url().'login/logout/'));
-				echo "<a href='$logout'>Logout</a>";
-		}else{
-				$login = $this->fb->sdk->getLoginUrl(array("scope"=>'email'));
-				echo "<a href='$login'>Login</a>";
-		}
-		
+   }
+
+   public function facebook(){
+        
+
+       
+
+        if($this->user_facebook){
+          try {
+                   $user_profile = $this->fb->sdk->api('/me'); // เป็นการเรียก Method /me ซึ่งเป็นข้อมูลเกี่ยวกับผู้ใช้ท่านนั้นๆ ที่ได้ทำการ Login
+                   echo "<br/>";
+                   print_r($user_profile);
+                   //echo $user_profile['email'];
+                   /*
+                   $_SESSION['LOGIN_FB_ID'] = $FB_ME_INFO["id"];
+                   $_SESSION['LOGIN_FB_FULLNAME'] = $FB_ME_INFO["name"];
+                   header("Location:./index.php");
+                   */
+          } catch(FacebookApiException $e) {
+
+               echo $e;  // print Error
+               $this->user_facebook = null;
+               //header("Location:./index.php?Login=fail");
+          }
+        }
+
+        if($this->user_facebook){
+            $logout = $this->fb->sdk->getLogoutUrl(array("next"=>base_url().'login/logout/'));
+            echo "<a href='$logout'>Logout</a>";
+        }else{
+            redirect($this->fb->sdk->getLoginUrl(array("scope"=>'email')));
+        }
+   }
+
+   public function twitter(){
+        var_dump($_POST);
+   }
+
+   public function form(){
+        var_dump($_POST);
    }
    
    public function logout(){
-   		session_destroy();
-		redirect(base_url().'login');
+        session_destroy();
+        redirect(base_url());
    }
 
 
--- application/views/include/modal.php
+++ application/views/include/modal.php
@@ -8,26 +8,53 @@
         <h4 class="modal-title" id="myModalLabel">Sign In</h4>
       </div>
       <div class="modal-body">
-      	<p class="text-center">Sign in using your registered account:</p>
-      	<div class="form-group">
-	        <div class="input-group">
-			  <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-			  <input type="email" class="form-control" placeholder="E-mail">
-			</div>
-		</div>
-		
-		<div class="form-group">
-	        <div class="input-group">
-			  <span class="input-group-addon"><i class="fa fa-key"></i></span>
-			  <input type="password" class="form-control" placeholder="Password">
-			</div>
-		</div>
-		
+          <form method="post" action="<?php echo base_url().'/login/form' ?>">
+              <div class="row">
+                <p class="text-center">Sign in using your registered account:</p>
+              </div>
+            <div class="row">
+                    <div class="form-group col-md-8 col-md-offset-2">
+                        <div class="input-group">
+                              <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                              <input type="email" name="email" class="form-control" placeholder="E-mail">
+                            </div>
+                    </div>
+            </div>
+
+            <div class="row">
+                    <div class="form-group col-md-8 col-md-offset-2">
+                        <div class="input-group">
+                              <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                              <input type="password" name="password" class="form-control" placeholder="Password">
+                        </div>
+                    </div>
+            </div>
+
+            <div class="row">
+                <div class="form-group col-md-8 col-md-offset-2">
+                    <button type="submit" class="btn btn-warning col-md-12 col-xs-12">Login</button>
+                </div>
+            </div>
+        </form>
+
+            <div class="row">
+                <div class="form-group col-md-8 col-md-offset-2">
+                    <a href="<?php echo base_url().'/login/twitter' ?>" class="btn btn-info col-md-12 col-xs-12"><i class="fa fa-twitter fa-2x"></i> Login with Twitter</a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="form-group col-md-8 col-md-offset-2">
+                    <a href="<?php echo base_url().'/login/facebook' ?>" class="btn btn-primary col-md-12 col-xs-12"><i class="fa fa-facebook-square fa-2x"></i> Login with Facebook</a>
+                </div>
+            </div>
       </div>
       <!--
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+          <div class="row">
+            
+            
+          </div>
       </div>
       -->
     </div><!-- /.modal-content -->
